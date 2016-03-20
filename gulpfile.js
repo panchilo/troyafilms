50f7bb5cf85eada0f960fb081a3c8f1e9fc9b60f
Francisco Lopez <panchilo@gmail.com>
2016-03-20 19:03:48 -0400

    Added autorefresh support with bowerSync

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
 var cssmin = require("gulp-cssmin");
 var rename = require("gulp-rename");
+var browserSync = require('browser-sync').create();
 
 var conf = {
 	src: "src/",
@@ -20,42 +21,49 @@ gulp.task("clean", function () {
 		.pipe(clean({force: true}));
 });
 
-gulp.task("copy:root", ["clean"], function () {
+gulp.task("copy:root", function () {
 	return gulp.src(path.join(conf.src, "*.*"))
 		.pipe(gulp.dest(conf.dist));
 });
 
-gulp.task("copy:images", ["clean"], function () {
+gulp.task("copy:images", function () {
 	return gulp.src(path.join(conf.src, "images/**/*.*"))
 		.pipe(gulp.dest(path.join(conf.dist, "images")));
 });
 
-gulp.task("copy", ["clean", "copy:root", "copy:images"]);
-
-gulp.task("components", ["clean"], function () {
-	return gulp.src([
-		path.join(conf.src, "components/**/*.min.css")
-		])
-		.pipe(gulp.dest(path.join(conf.dist, "/styles")));
+gulp.task("copy:components", function () {
+	return gulp.src(path.join(conf.src, "components/**/*.min.css"))
+		.pipe(gulp.dest(path.join(conf.dist, "/styles")))
+		.pipe(browserSync.stream());
 });
 
-gulp.task("css", ["clean"], function () {
-	return gulp.src([
-		path.join(conf.src, "styles/**/*.sass"),
-		path.join(conf.src, "styles/**/*.scss"),
-		path.join(conf.src, "styles/**/*.css")
-		])
+gulp.task("copy", ["copy:root", "copy:images", "copy:components"]);
+
+gulp.task("css", function () {
+	return gulp.src(path.join(conf.src, "styles/**/*.{sass,scss,css}"))
 		.pipe(sass().on("error", sass.logError))
 		.pipe(autoprefixer({ browsers: ["last 10 versions"] }))
 		.pipe(cssmin())
 		.pipe(rename({suffix: ".min"}))
-		.pipe(gulp.dest(path.join(conf.dist, "/styles")));
+		.pipe(gulp.dest(path.join(conf.dist, "/styles")))
+		.pipe(browserSync.stream());
 });
 
-gulp.task("css:watch", function () {
-	gulp.watch(path.join(conf.src, "styles/**/*.sass"), ["css"]);
-});
-
-gulp.task("default", ["clean", "copy", "components", "css"], function() {
+gulp.task("default", ["copy", "css"], function() {
 	// place code for your default task here
+});
+
+gulp.task('watch:root', ['copy:root'], function () { browserSync.reload(); });
+gulp.task('watch:images', ['copy:images'], function () { browserSync.reload(); });
+
+gulp.task("serve", ["default"], function() {
+    browserSync.init({
+        server: conf.dist
+    });
+
+    gulp.watch("src/styles/**/*.{sass,scss,css}", ['css']);
+    gulp.watch("src/components/**/*.min.css", ['copy:components']);
+
+    gulp.watch(path.join(conf.src, "*.*"), ["watch:root"]);
+    gulp.watch(path.join(conf.src, "images/**/*.*"), ["watch:images"]);
 });
