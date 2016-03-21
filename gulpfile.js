86b29fc674d3244888bd28227803a85a7c86c1f1
Francisco Lopez <panchilo@gmail.com>
2016-03-20 20:20:16 -0400

    Added ftp publish support with vinyl-ftp. Use 'gulp publish'

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,16 @@
 
 var path = require("path");
 var gulp = require("gulp");
+var gutil = require("gulp-util");
 var clean = require("gulp-clean");
 var concat = require("gulp-concat");
 var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
 var cssmin = require("gulp-cssmin");
 var rename = require("gulp-rename");
-var browserSync = require('browser-sync').create();
+var browserSync = require("browser-sync").create();
+var ftp = require("vinyl-ftp");
+var ftpConfig = require("./.ftpconfig");
 
 var conf = {
 	src: "src/",
@@ -49,21 +52,38 @@ gulp.task("css", function () {
 		.pipe(browserSync.stream());
 });
 
-gulp.task("default", ["copy", "css"], function() {
+gulp.task("build", ["copy", "css"], function() {
 	// place code for your default task here
 });
 
-gulp.task('watch:root', ['copy:root'], function () { browserSync.reload(); });
-gulp.task('watch:images', ['copy:images'], function () { browserSync.reload(); });
+gulp.task("watch:root", ["copy:root"], function () { browserSync.reload(); });
+gulp.task("watch:images", ["copy:images"], function () { browserSync.reload(); });
 
-gulp.task("serve", ["default"], function() {
+gulp.task("serve", ["build"], function() {
     browserSync.init({
         server: conf.dist
     });
 
-    gulp.watch("src/styles/**/*.{sass,scss,css}", ['css']);
-    gulp.watch("src/components/**/*.min.css", ['copy:components']);
+    gulp.watch("src/styles/**/*.{sass,scss,css}", ["css"]);
+    gulp.watch("src/components/**/*.min.css", ["copy:components"]);
 
     gulp.watch(path.join(conf.src, "*.*"), ["watch:root"]);
     gulp.watch(path.join(conf.src, "images/**/*.*"), ["watch:images"]);
 });
+
+gulp.task("publish", ["build"], function () {
+	var ftpConn = ftp.create({
+		host: ftpConfig.host || "ftp.bluealba.com",
+		user: ftpConfig.user || "bluealba",
+		password: ftpConfig.config.pass,
+		parallel: 3,
+		log: gutil.log
+	});
+	return gulp.src(path.join(conf.dist, "**/*"), { base: conf.dist, buffer: false})
+		.pipe(ftpConn.newer("/public_html"))
+		.pipe(ftpConn.dest("/public_html"));
+
+});
+
+gulp.task("default", ["build"]);
+
